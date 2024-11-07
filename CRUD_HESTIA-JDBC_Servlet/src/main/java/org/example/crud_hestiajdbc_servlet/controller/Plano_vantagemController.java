664d89526b03b6d3a876b52c8a6cd227f6223680
Laurente64 <lucas.laurente@germinare.org.br>
2024-11-06 23:01:42 -0300

    Acho que foi

--- CRUD_HESTIA-JDBC_Servlet/src/main/java/org/example/crud_hestiajdbc_servlet/controller/Plano_vantagemController.java
+++ CRUD_HESTIA-JDBC_Servlet/src/main/java/org/example/crud_hestiajdbc_servlet/controller/Plano_vantagemController.java
@@ -105,13 +105,23 @@ public class Plano_vantagemController extends HttpServlet
         {
             String vantagem = vantagemParameter;
             char ativo      = ativoParameter.charAt(0);
-            String nmPlano  = nomePlanoParameter;
-            Plano_vantagem planoVantagem = new Plano_vantagem(vantagem, ativo, nmPlano);
+            String nomePlano  = nomePlanoParameter;
+            Plano_vantagem planoVantagem = new Plano_vantagem(vantagem, ativo, nomePlano);
 
             if (planoVantagemDAO.addPlanoVantagem(planoVantagem) > 0)
+            {
                 Utils.logSuccessfulCreation(req);
+
+                // Chama o método de leitura, que obtém os registros do banco e responde a requisição com o parâmetro
+                readPlano_vantagem(req, resp, nomePlano);
+
+                // Retorna para não chamar o dispacher novamente
+                return;
+            }
             else
+            {
                 Utils.logDatabaseIssue(req);
+            }
         }
         else
         {
@@ -242,6 +252,36 @@ public class Plano_vantagemController extends HttpServlet
         req.getRequestDispatcher("Crud.jsp").forward(req, resp);
     }
 
+    // Sobrecarga que retorna as vantagens de mesmo plano
+    private void readPlano_vantagem(HttpServletRequest req, HttpServletResponse resp, String nomePlanoParameter) throws ServletException, IOException
+    {
+        // Verifica se o parâmetro do nome é válido
+        if (Utils.isValidString(nomePlanoParameter))
+        {
+            String nomePlano = nomePlanoParameter;
+            ResultSet list = planoVantagemDAO.getPlanoVantagensByNmPlano(nomePlano);
+
+            if (list != null)
+            {
+                req.setAttribute("parent-table-name", nomePlano);
+
+                req.setAttribute("list", Utils.toPlano_vantagemStringList(list));
+                Utils.logSuccessfulReading(req);
+            }
+            else
+            {
+                Utils.logDatabaseIssue(req);
+            }
+        }
+        else
+        {
+            Utils.logInputSetback(req);
+        }
+
+        // Redireciona a requisição e resposta de volta à página de administração
+        req.getRequestDispatcher("Crud.jsp").forward(req, resp);
+    }
+
     private void updatePlano_vantagem(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
         // Recupera parâmetros da requisão e os amarzena nas variáveis correspondentes
@@ -263,13 +303,23 @@ public class Plano_vantagemController extends HttpServlet
             UUID codigo     = UUID.fromString(codigoParameter);
             String vantagem = vantagemParameter;
             char ativo      = ativoParameter.charAt(0);
-            String nmPlano = nomePlanoParameter;
-            Plano_vantagem planoVantagem = new Plano_vantagem(codigo, vantagem, ativo, nmPlano);
+            String nomePlano = nomePlanoParameter;
+            Plano_vantagem planoVantagem = new Plano_vantagem(codigo, vantagem, ativo, nomePlano);
 
             if (planoVantagemDAO.updatePlanoVantagem(planoVantagem) > 0)
+            {
                 Utils.logSuccessfulUpdate(req);
+
+                // Chama o método de leitura, que obtém os registros do banco e responde a requisição com o parâmetro
+                readPlano_vantagem(req, resp, nomePlano);
+
+                // Retorna para não chamar o dispacher novamente
+                return;
+            }
             else
+            {
                 Utils.logDatabaseIssue(req);
+            }
         }
         else
         {
@@ -282,18 +332,35 @@ public class Plano_vantagemController extends HttpServlet
 
     private void deletePlano_vantagem(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
-        // Recupera o parâmetro da requisão e o amarzena
+        // Recupera parâmetros da requisão e os amarzena
         String codigoParameter = req.getParameter("uId");
+        String nomePlanoParameter = req.getParameter("cNmPlano");
 
         // Verifica se os parâmetros retornaram valores válidos
-        if (Utils.isValidUUID(codigoParameter))
+        if
+        (
+                Utils.isValidUUID(codigoParameter)      &&
+                Utils.isValidString(nomePlanoParameter)
+        )
         {
             UUID codigo = UUID.fromString(codigoParameter);
+            String nomePlano = nomePlanoParameter;
 
             if (planoVantagemDAO.removePlanoVantagem(codigo) > 0)
+            {
                 Utils.logSuccessfulRemoval(req);
+
+                // Chama o método de leitura, que obtém os registros do banco e responde a requisição com o parâmetro
+                readPlano_vantagem(req, resp, nomePlano);
+
+                // Retorna para não chamar o dispacher novamente
+                return;
+            }
             else
+            {
                 Utils.logDatabaseIssue(req);
+
+            }
         }
         else
         {
